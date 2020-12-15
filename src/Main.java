9f7df591a1355c8cb5ead9e32546c601c2f64e9b
Yuliya Kurbatova <j53781551@gmail.com>
2020-12-15 21:12:08 +0300

    Main added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // стоимость в копейках
+        long cost = 2_454_050;
+        long bonus_miles = cost / 2000;
+        System.out.println(bonus_miles);
+    }
+}
